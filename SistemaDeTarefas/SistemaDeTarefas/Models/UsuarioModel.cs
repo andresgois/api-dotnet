b40cf218cbde4e16178878e2cb38f2083c0771fd
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-15 22:16:23 -0300

    iniciando novo sistemas

---
+++ SistemaDeTarefas/SistemaDeTarefas/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class UsuarioModel
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
