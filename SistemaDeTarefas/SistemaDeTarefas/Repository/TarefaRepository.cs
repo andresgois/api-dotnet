7088d3215b115838b15913b747acb93a58419d59
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-17 20:36:19 -0300

    ⭐ feat(scope):  criando repository de tarefas

---
+++ SistemaDeTarefas/SistemaDeTarefas/Repository/TarefaRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repository.interfaces;
+
+namespace SistemaDeTarefas.Repository
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private readonly SistemasTarefasDBContext _dbContext;
+
+        public TarefaRepository(SistemasTarefasDBContext sistemasTarefasDBContext) 
+        { 
+            _dbContext = sistemasTarefasDBContext;
+        }
+
+        public async Task<TarefaModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Tarefas.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<TarefaModel>> BuscarTodosTarefas()
+        {
+            return await _dbContext.Tarefas.ToListAsync();
+        }
+
+        public async Task<TarefaModel> Adicionar(TarefaModel tarefa)
+        {
+            await _dbContext.Tarefas.AddAsync(tarefa);
+            await _dbContext.SaveChangesAsync();
+            return tarefa;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            TarefaModel tarefaPorId = await BuscarPorId(id);
+            if (tarefaPorId == null)
+            {
+                throw new Exception($"Tarefa com ID: {id} não econtrado!");
+            }
+            _dbContext.Tarefas.Remove(tarefaPorId);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<TarefaModel> Atualizar(TarefaModel tarefa, int id)
+        {
+            TarefaModel tarefaPorId = await BuscarPorId(id);
+            if(tarefaPorId == null)
+            {
+                throw new Exception($"Usuário com ID: {id} não econtrado!");
+            }
+            tarefaPorId.Name = tarefa.Name;
+            tarefaPorId.Status = tarefa.Status;
+            tarefaPorId.Description = tarefa.Description;
+            tarefaPorId.UsuarioId = tarefa.UsuarioId;
+
+            _dbContext.Tarefas.Update(tarefaPorId);
+            await _dbContext.SaveChangesAsync();
+            return tarefaPorId;
+        }
+
+    }
+}
---
+++ SistemaDeTarefas/SistemaDeTarefas/Repository/interfaces/ITarefaRepository.cs
@@ -0,0 +1,18 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repository.interfaces
+{
+    public interface ITarefaRepository
+    {
+        Task<List<TarefaModel>> BuscarTodosTarefas();
+
+        Task<TarefaModel> BuscarPorId(int id);
+
+        Task<TarefaModel> Adicionar(TarefaModel tarefa);
+
+        Task<TarefaModel> Atualizar(TarefaModel tarefa, int id);
+
+        Task<bool> Apagar(int id);
+
+    }
+}
