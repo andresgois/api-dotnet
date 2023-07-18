c0db4b50af508c90da2db7b37baa7e2fd2ec1a5b
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-17 21:29:08 -0300

    ⭐ feat(scope):  adicionando controller do viacep

---
+++ SistemaDeTarefas/SistemaDeTarefas/Controllers/CepController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Integracao.Interfaces;
+using SistemaDeTarefas.Integracao.Response;
+
+namespace SistemaDeTarefas.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly IViaCepIntegracao _integracao;
+
+        public CepController(IViaCepIntegracao integracao)
+        {
+            _integracao = integracao;
+        }
+
+        [HttpGet("{cep}")]
+        public async Task<ActionResult<ViaCepResponse>> ListaDeDadosDeEndereco(string cep) 
+        { 
+            var responseDate = await _integracao.ObterDadosViaCep(cep);
+
+            if(responseDate == null)
+            {
+                return BadRequest("Cep não encontrado");
+            }
+            return Ok(responseDate);
+        }
+    }
+}
