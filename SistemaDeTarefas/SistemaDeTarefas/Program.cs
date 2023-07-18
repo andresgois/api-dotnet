720bcedccc98df6c7a8e6e6ce80a69ee66d5235f
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-17 21:20:17 -0300

    ⭐ feat(scope):  configuracao de integracao com viacep

--- SistemaDeTarefas/SistemaDeTarefas/Program.cs
+++ SistemaDeTarefas/SistemaDeTarefas/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Refit;
 using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Integracao;
+using SistemaDeTarefas.Integracao.Interfaces;
+using SistemaDeTarefas.Integracao.Refit;
 using SistemaDeTarefas.Repository;
 using SistemaDeTarefas.Repository.interfaces;
 
@@ -25,6 +29,12 @@ namespace SistemaDeTarefas
                 );
             builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+            builder.Services.AddScoped<IViaCepIntegracao, ViaCepIntegracao>();
+
+            builder.Services.AddRefitClient<IViaCepIntegracaoRefit>().ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new Uri("https://viacep.com.br");
+            });
 
 
             var app = builder.Build();
